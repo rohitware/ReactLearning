d6a101451ae24da3a1dee3fee792660a66d4d14d
Rohit Ware <rohitware99@gmail.com>
2025-04-21 21:15:13 +0530

    mounting that component into the application means in app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import SimpleCalculator from './components/SimpleCalculator';
 import MathOperations from './components/MathOperations';
 import Counter from './components/Counter';
 import Child from './components/childComponent';
+import StateInFuncComp from './StateInReact/StateInFuncComp';
+import Body1 from './propsInReact/Body1';
+import Body2 from './propsInReact/Body2';
+import Header from './propsInReact/Header';
+import Footer from './propsInReact/Footer';
+import Account from './propsInReact/Account';
 
 function App() {
   const message = "I am from the Parent Component";
+
+  const companyName = "Zest india it services";
 
   return (
     <>
@@ -16,7 +24,17 @@ function App() {
       <SimpleCalculator />
       <MathOperations />
       <Counter />
+      <Header compName={companyName} CompanyLoc={"pune"} />
+      <Body1 mesg={"Welcome to Body 11"} />
+      <Body2 />
+
+      <Account />
+
+      <Footer compName={companyName} CompanyLoc={"pune"} />
+
       <Child greeting={message} />
+
+      <StateInFuncComp />
     </>
   );
 }
